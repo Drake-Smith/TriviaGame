80d3e040c23a2d0644a8f41e79f38f8b7421236e
Drake-Smith <drakesmithjr@gmail.com>
2016-08-04 18:13:50 -0700

    final push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,11 @@
 //declare variables
-var timeLeft = 10;
-var handler;
-var inbetweenQues;
-var userAnswer;
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
-var quesNumber=0;
-var i;
-var answerKey;
+var timeLeft = 12; //starts off at 12 seconds
+var handler; //var for the timer
+var correct = 0; //how many questions guessed correctly
+var incorrect = 0; //how many questions guessed incorrectly
+var unanswered = 0; //how many ques wrong
+var quesNumber = 0; //which question we're currently at
+var howMany = 1; //which question # (of 12) we're at to display on the screen
 
 //object containing the questions
 var questions = {
@@ -29,11 +26,11 @@ var questions = {
 
 	choices:[
 			['Gryffindor', 'Slytherin', 'Ravenclaw', 'Hufflepuff'],
-	 		['Wingardium Leviosa', 'Alohomora', 'Expelliarmus', 'Lumos'],
+	 		['Wingardium Leviosa', 'Alohomora', 'Expelliarmus', 'Open Seasame'],
 	 		['Drogon', 'Norbert', 'Smaug', 'Snuff'],
-	 		["Hedwig", "Pigwidgeon", "Errol", "Nyla"],
+	 		["Hedwig", "Pigwidgeon", "Olga", "Nyla"],
 	 		["Beater", "Keeper", "Seeker", "He rode the bench"],
-	 		["Fiero", "Magentus", "Ashner", "Fawkes"],
+	 		["Fiero", "Magentus", "Chad", "Fawkes"],
 	 		["Cat", "Frog", "Owl", "Mouse"],
 	 		["Imperius", "Cruciatus", "Avada Kedavra", "Sectumsempra"],
 	 		["John Puzzle", "Tom Riddle", "Jack Stumper", "Rick BrainTeaser"],
@@ -55,32 +52,41 @@ var questions = {
 			"Gilderoy Lockhart",
 			"Stag",
 			"Nicholas Flamel"
+			],
+
+	image: [
+			'assets/images/hufflepuff.jpeg',
+			'assets/images/alohomora.jpeg',
+			'assets/images/norbert.jpg',
+			'assets/images/hedwig.jpg',
+			'assets/images/seeker.jpg',
+			'assets/images/fawkes.jpg',
+			'assets/images/cat.png',
+			'assets/images/avada.jpg',
+			'assets/images/tomriddle.jpg',
+			'assets/images/lockhart.jpg',
+			'assets/images/stag.png',
+			'assets/images/stone.jpg'
 			]
 	}
 
 $(document).ready(function(){
 
+	//click event for picking your choice
 	function attachEvents() {
 		$('.choices').on('click', function() {
-			var yourAnswer = $(this).data('name');
+			var yourAnswer = $(this).attr('data-name');
+			console.log("i clicked: " + yourAnswer);
 
 			if (yourAnswer === questions.answer[quesNumber]) {
 				correctAnswer();
+
 			} else {
 				wrongAnswer();
 			}
 		});
-
-		// //clicking start button starts Game
-		// $('#start').on('click', startGame);
 	};
 	attachEvents();
-
-
-
-		// //clicking start button starts Game
-		// $('#start').on('click', startGame);
-	// };
 
 	//clicking start button starts Game
 	$('#start').on('click', startGame);
@@ -90,8 +96,8 @@ $(document).ready(function(){
 		$("#start").attr("style","display:none");
 		reset();
 		$("#timeLeft").html("Time Remaining: " + timeLeft + " seconds");
+		$("#howMany").html("Question: " + howMany + " of 12")
 		$("#final-results").empty();
-		//i++;
 		question(quesNumber);
 	}
 
@@ -111,11 +117,13 @@ $(document).ready(function(){
 		timeRunOut(handler);
 	}
 
+	//reset timer to 12 seconds
 	function reset() {
-		timeLeft = 10;
+		timeLeft = 12;
 		$("#timeLeft").html("Time Remaining: " + timeLeft + " seconds");
 	}
 
+	//decrease timer by 1
 	function decrement() {
 		timeLeft--;
 		$("#timeLeft").html("Time Remaining: " + timeLeft + " seconds");
@@ -124,144 +132,138 @@ $(document).ready(function(){
 		}
 	}
 
+	//one question turn
 	function question(num) {
 
 		console.log(questions.ques[num]);
 		console.log(questions.choices[num]);
 		console.log(questions.answer[num]);
+
+		//reset and initialize timer
 		reset();
 		timer();
 
+		//empty the previous question info
 		$("#answer").empty();
- 
-		$(".question").html("<h3>"+ questions.ques[num]+"</h3>");
-		$("#choice1").html(questions.choices[num][0]);
-		$("#choice2").html(questions.choices[num][1]);
-		$("#choice3").html(questions.choices[num][2]);
-		$("#choice4").html(questions.choices[num][3]);
+		$('.photo').empty();
 
-		// $('#choice1').removeData('data-name', questions.choices[i-1][0]);
-		// $('#choice2').removeData('data-name', questions.choices[i-1][1]);
-		// $('#choice3').removeData('data-name', questions.choices[i-1][2]);
-		// $('#choice4').removeData('data-name', questions.choices[i-1][3]);
+		$(".question").html("<h2>"+ questions.ques[num]+"</h2>");
+		
+		$('.choices').show();
 
+		//show questions on screen + create hover effect 
+		$("#choice1").html(questions.choices[num][0]).hover(function(){
+    		$(this).css("background-color", "red");
+   		 	}, function(){
+    			$(this).css("background-color", "transparent");
+				});
 
+		$("#choice2").html(questions.choices[num][1]).hover(function(){
+    		$(this).css("background-color", "red");
+   		 	}, function(){
+    			$(this).css("background-color", "transparent");
+				});
+		$("#choice3").html(questions.choices[num][2]).hover(function(){
+    		$(this).css("background-color", "red");
+   		 	}, function(){
+    			$(this).css("background-color", "transparent");
+				});
+		$("#choice4").html(questions.choices[num][3]).hover(function(){
+    		$(this).css("background-color", "red");
+   		 	}, function(){
+    			$(this).css("background-color", "transparent");
+				});
+
+		//attribute question choice data to each choice 
 		$('#choice1').attr('data-name', questions.choices[num][0]);
 		$('#choice2').attr('data-name', questions.choices[num][1]);
 		$('#choice3').attr('data-name', questions.choices[num][2]);
 		$('#choice4').attr('data-name', questions.choices[num][3]);
 		
-		//answerKey = questions.answer[quesNumber];
-
-		// $('.choices').on('click', function(){
-		// 	userAnswer = $(this).attr('data-name');
-			
-		// 	if (userAnswer === answerKey) {
-		// 		correctAnswer(i);
-		// 	}
-			
-		// 	if (userAnswer !== answerKey){
-		// 		wrongAnswer(i);
-		// 	}
-		// });
-
-		// if (timeLeft === 0){
-		// 	timeRunOut(i);
-		// }	
+		$("#howMany").html("Question: " + howMany + " of 12")
 	}
 
-
-// $('#choice2').on('click', checkIfRight(i));
-// $('#choice3').on('click', checkIfRight(i));
-// $('#choice4').on('click', checkIfRight(i));
-
-
-//$('#choice1').on('click', correctAnswer);
-// function checkIfRight(i) {
-// 	userAnswer = $(this).attr('data-name');
-// 	if (userAnswer === questions.answer[i]) {
-// 	correctAnswer();
-// }
-// else {
-// 	wrongAnswer();
-// }
-
-// }
-
+	//if question is correct
 	function correctAnswer() {
-		$('.choices').empty();
+		$('.choices').empty().hide(); //hide() prevents hover effect from still working even when choices arent visibile
 		$('.question').empty();
-		$('#answer').text("Correct answer!");
 
+		$('#answer').html("<h2> Correct!");
+		$('.photo').html('<img src="' + questions.image[quesNumber] + '" />');
+
+		//increments
+		howMany++;
 		correct++;
-		console.log("correct: " + correct);
 		quesNumber++;
+		console.log("correct: " + correct);
 		console.log("quesNum: " + quesNumber);
 		pause();
-		
-	
+
+		//check if we're at the last question
 		if (checkQuestions(quesNumber)){
 			setTimeout(function(){
 				gameOver();}, 4000);
 		}
-		
 		else {
 			setTimeout(function(){
 				question(quesNumber);}, 4000);
-			// reset();
 		}
 	}
 
+	//if wrong answer
 	function wrongAnswer() {
 	
-		$('.choices').empty();
+		$('.choices').empty().hide();
 		$('.question').empty();
-		$('#answer').text("Wrong answer!");
-	
-		incorrect++;
-		console.log("incorrect: " + incorrect);
-		quesNumber++;
+		$('#answer').html("<h2>Wrong! The correct answer is " + questions.answer[quesNumber] + ".");
+		$('.photo').html('<img src="' + questions.image[quesNumber] + '" />');
 
+		howMany++;
+		incorrect++;
+		quesNumber++;
+		console.log("incorrect: " + incorrect);
 		console.log("quesNum: " + quesNumber);
+
 		pause();
 
 		if (checkQuestions(quesNumber)){
 			setTimeout(function(){
 				gameOver();}, 4000);
 		}
-		
 		else {
 			setTimeout(function(){
 				question(quesNumber);}, 4000);
-			// reset();
 		}	
 	}
 
+	//if user does not answer and timer runs out
 	function timeRunOut() {
 	
-		$('.choices').empty();
+		$('.choices').empty().hide();
 		$('.question').empty();
-		$('#answer').text("Time ran up!");
+		$('#answer').html("<h2>Time's up! The correct answer is " + questions.answer[quesNumber] + ".");
+		$('.photo').html('<img src="' + questions.image[quesNumber] + '" />');
 	
+		howMany++;
 		unanswered++;
-		console.log("unanswered: " + unanswered);
 		quesNumber++;
 
+		console.log("unanswered: " + unanswered);
 		console.log("quesNum: " + quesNumber);
-			pause();
+		
+		pause();
 	
 		if (checkQuestions(quesNumber)){
 			setTimeout(function(){
 				gameOver();}, 4000);
 		}
-		
 		else {
 			setTimeout(function(){
 				question(quesNumber);}, 4000);
-			// reset();
 		}
 	}
 
+	//function for if question is the last one 
 	function checkQuestions(number){
 		if (number >= questions.ques.length){
 			return true;
@@ -271,26 +273,26 @@ $(document).ready(function(){
 		}
 	}
 
+	//game over empties everything, resets integer counter variables, and shows final stats
 	function gameOver() {
 		$('.choices').empty();
 		$('.question').empty();
 		$('#answer').empty();
 		$("#timeLeft").empty();
+		$("#howMany").empty();
+		$('.photo').empty();
+
 		$("#start").attr("style","display:inline");
+		$(".question").append("<h2> Thanks for playing. Here's how you did.");
 		$(".question").append("<h3> Correct: " + correct);
 		$(".question").append("<h3> Incorrect: " + incorrect);
 		$(".question").append("<h3> Unanswered: " + unanswered);
-		$("#final-results").append("<h4> Play again? Press start!");
-		// $("#final-results").append("<button>Play again?");
+		$("#final-results").append("<h2> Play again? Press start!");
 		quesNumber = 0;
 		correct = 0;
 		incorrect = 0;
 		unanswered = 0;
-
-
+		howMany = 1;
 	}
-
-//need some sort of setTimeout in between questions too , create a function for setTimeout to call
-//need attr data to attribute the answer to each div. then, can use that to compare later on
 });
 
